9ac2768a294675564fca233e2c91349d50855339
Vlad Zhukov <me@vladzhukov.ru>
2023-07-10 20:33:45 +0300

    Prerelease

--- packages/vendure-order-code-strategy/src/index.ts
+++ packages/vendure-order-code-strategy/src/index.ts
@@ -1,10 +1,50 @@
-import { type OrderCodeStrategy } from '@vendure/core';
-import { init } from '@paralleldrive/cuid2';
+import {
+    type OrderCodeStrategy,
+    Injector,
+    Order,
+    RequestContext,
+    TransactionalConnection,
+    InternalServerError,
+} from '@vendure/core';
+import { customAlphabet } from 'nanoid';
 
-const createId = init({ length: 8 });
+const nanoId = customAlphabet('0123456789', 6);
+
+function createId(prefix = 'R') {
+    return prefix + nanoId();
+}
 
 export class ShortOrderCodeStrategy implements OrderCodeStrategy {
-    generate() {
-        return createId().toUpperCase();
+    private connection: TransactionalConnection | undefined;
+
+    init(injector: Injector) {
+        this.connection = injector.get(TransactionalConnection);
+
+        console.log('init', this.connection);
+    }
+
+    async generate(ctx: RequestContext): Promise<string> {
+        let code = createId();
+
+        console.log('generate', this.connection);
+
+        if (this.connection) {
+            for (let i = 0; i++; ) {
+                // Max loop reached
+                if (i >= 25) {
+                    throw new InternalServerError("Couldn't generate a valid order code");
+                }
+
+                const orderExists = await this.connection
+                    .getRepository(ctx, Order)
+                    .count({ where: { code } });
+                if (orderExists === 0) {
+                    break;
+                }
+                code = createId();
+            }
+        }
+
+        return code;
     }
 }
